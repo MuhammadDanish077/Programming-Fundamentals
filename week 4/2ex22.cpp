87f0c1f14ed2160d78a54bb6fa22a327feb83a2e
MuhammadDanish077 <m.danish.125690@gmail.com>
2026-03-08 16:05:20 +0500

    week 4

---
+++ week 4/2ex22.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+char going;
+cout<< "Are your friends going? (press y for yes , n for no: ";
+cin>> going;
+if (going =='y'){
+cout<<"you are also going!";
+}
+ if(going=='n'){
+cout<<"You are not going!";}
+
+ }
