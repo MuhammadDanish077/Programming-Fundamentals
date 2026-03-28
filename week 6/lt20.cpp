c96328e2814cb38c30f3fada48e42438573396ff
MuhammadDanish077 <m.danish.125690@gmail.com>
2026-03-28 23:49:19 +0500

    week 6

--- week 6/lt20.cpp
+++ week 6/lt20.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
-int main()
-{  
-  int n; cin>>n;
-  string no[n];
-  
-  for (int i=0; i<n; i++){
-    cin>>no[i];
-    char l = no[i][no[i].length() - 1];
-    if(l != '7'){
-      no[i]+="7";
-    } 
-  }  
-  for (int i=0; i<n; i++){
-  cout<< no[i]<< endl;
-}}
+
+void jazzify(string chords[], int size) {
+    for (int i = 0; i < size; i++) {
+        string &current = chords[i];
+        if (!current.empty() && current.back() != '7') {
+            current += "7";
+        }
+    }
+}
+
+int main() {
+    int total;
+    cout << "Enter the number of chords: ";
+    cin >> total;
+
+    string chordList[100]; 
+
+    cout << "Enter " << total << " chords, one per line:" << endl;
+    for (int i = 0; i < total; i++) {
+        cin >> chordList[i];
+    }
+
+    jazzify(chordList, total);
+
+    cout << "Jazzified chords: [";
+    for (int i = 0; i < total; i++) {
+        cout << chordList[i];
+        if (i < total - 1) {
+            cout << ", "; 
+        }
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
